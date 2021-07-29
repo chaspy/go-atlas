8000a5ce90075c7deb1c4204c0f72e8b899f3f9e
kondo takeshi <chaspy@users.noreply.github.com>
2021-07-29 22:33:38 +0900

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/chaspy/go-atlas
+
+go 1.16
+
+require (
+	github.com/mongodb-forks/digest v1.0.2
+	go.mongodb.org/atlas v0.10.1
+)
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/mongodb-forks/digest"
+	"go.mongodb.org/atlas/mongodbatlas"
+)
+
+func main() {
+	publicKey := os.Getenv("MONGODB_ATLAS_PUBLIC_KEY")
+	privateKey := os.Getenv("MONGODB_ATLAS_PRIVATE_KEY")
+	t := digest.NewTransport(publicKey, privateKey)
+	tc, err := t.Client()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	client := mongodbatlas.NewClient(tc)
+	projects, _, err := client.Projects.GetAllProjects(context.Background(), nil)
+
+	fmt.Printf("%s", projects)
+}
