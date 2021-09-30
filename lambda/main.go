6c5cc3b96f79d4090ce16ea59fedaf38d834aa56
kondo takeshi <chaspy@users.noreply.github.com>
2021-10-01 01:23:52 +0900

    Add main

---
+++ lambda/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+
+	"context"
+	"github.com/aws/aws-lambda-go/lambda"
+	"log"
+	"os"
+
+	"github.com/mongodb-forks/digest"
+	"go.mongodb.org/atlas/mongodbatlas"
+)
+
+func hello() {
+	publicKey := os.Getenv("MONGODB_ATLAS_PUBLIC_KEY")
+	privateKey := os.Getenv("MONGODB_ATLAS_PRIVATE_KEY")
+	t := digest.NewTransport(publicKey, privateKey)
+	tc, err := t.Client()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	client := mongodbatlas.NewClient(tc)
+
+	ctx := context.Background()
+	clusterName := "test-source-cluster"
+	groupID := "6155d1572d865910d4137d3a"
+
+	cluster, _, err := client.Clusters.Get(ctx, groupID, clusterName)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	fmt.Printf("%#v", cluster)
+}
+
+func main() {
+	lambda.Start(hello)
+}
